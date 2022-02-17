d6278bdb85a58889801045c50f3ac79debccac32
VladimirRiabov <97258826+VladimirRiabov@users.noreply.github.com>
2022-02-17 21:16:00 +0300

    Initial Commit

---
+++ Delegate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Delegate
+//
+//  Created by Владимир Рябов on 17.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
